7149616f83234c826d6af309121f49a919c61652
PressureDraper <warmarines00@hotmail.com>
2023-12-20 13:04:14 -0600

    add: activities get & post endpoints

--- src/middlewares/validate-fields.ts
+++ src/middlewares/validate-fields.ts
@@ -3,7 +3,6 @@ import { validationResult } from 'express-validator';
 
 export const validateFields = ( req: any, res: Response, next: NextFunction ) => {
     const errors = validationResult( req );
-    console.log(errors);
 
     if( !errors.isEmpty() ) {
         return res.status( 400 ).json({
