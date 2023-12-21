d760d8d2541b16c8301a4ae2f210dca9ab80e6c1
PressureDraper <warmarines00@hotmail.com>
2023-12-21 08:17:24 -0600

    add: activities put & delete endpoints

--- src/interfaces/activitiesQueries.ts
+++ src/interfaces/activitiesQueries.ts
@@ -12,3 +12,13 @@ export interface PropsCreateActivitiesQueries {
     deficiente: number,
     muy_deficiente: number
 }
+
+export interface PropsUpdateActivitiesQueries {
+    descripcion?: string,
+    excelente?: number,
+    bueno?: number,
+    regular?: number,
+    deficiente?: number,
+    muy_deficiente?: number,
+    activity_id: number
+}
