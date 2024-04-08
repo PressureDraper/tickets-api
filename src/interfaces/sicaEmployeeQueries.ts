f12a69850e7d96c006c0d1258b6d680c27cbbbc8
PressureDraper <warmarines00@hotmail.com>
2024-04-08 10:45:41 -0600

    add: sica employees endpoint

---
+++ src/interfaces/sicaEmployeeQueries.ts
@@ -0,0 +1,6 @@
+export interface SicaEmployeeQueries {
+    limit: string;
+    page: string;
+    enrollmentFilter: string;
+    nameFilter: string;
+}
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,17 +1,22 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaClient as PrismaClient2 } from '../../prisma/generated/client2';
 
 let db : PrismaClient;
+let db2: PrismaClient2;
 
 //! Check if we are running in production mode
 if( process.env.NODE_ENV == 'production' ) {
     db = new PrismaClient();
+    db2 = new PrismaClient2();
 } else {
     //! Check if is alredy connected to the database
     if( !global.db ) {
         global.db = new PrismaClient();
+        global.db2 = new PrismaClient2();
     }
 
     db = global.db;
+    db2 = global.db2;
 }
 
-export { db };
+export { db, db2 };
