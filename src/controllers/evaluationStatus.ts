ec287d250aa1727828b5c8e557fe086dca303a83
PressureDraper <warmarines00@hotmail.com>
2024-05-15 15:28:55 -0600

    add: seguimiento view

--- src/controllers/evaluationStatus.ts
+++ src/controllers/evaluationStatus.ts
@@ -1,10 +1,28 @@
 import { Response } from "express";
-import { getStatusEvaluationQuery } from "../helpers/evaluationStatusQueries";
+import { getResidentsPerStatus, getStatusEvaluationQuery } from "../helpers/evaluationStatusQueries";
 
 export const getStatusEvaluation = async (req: any, res: Response) => {
     try {
         const params: any = req.query;
         const queryEvaluation = await getStatusEvaluationQuery(params);
+        res.status(200).json({
+            ok: true,
+            msg: 'Ok',
+            data: queryEvaluation
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Server error contact the administrator'
+        });
+    }
+}
+
+export const getResidentsPerStatusData = async (req: any, res: Response) => {
+    try {
+        const params: any = req.query;
+        const queryEvaluation = await getResidentsPerStatus({ ...params });
         res.status(200).json({
             ok: true,
             msg: 'Ok',
--- src/helpers/evaluationStatusQueries.ts
+++ src/helpers/evaluationStatusQueries.ts
@@ -1,5 +1,5 @@
 import moment from "moment";
-import { ArrayResidentsEvaluation, PropsGetResidentsEvaluationQueries } from "../interfaces/evaluationStatusQueries";
+import { ArrayResidentsEvaluation, PropsGetResidentsEvaluationQueries, PropsGetResidentsPerStatusQueries } from "../interfaces/evaluationStatusQueries";
 import { db } from "../utils/db";
 
 export const getStatusEvaluationQuery = async (data: ArrayResidentsEvaluation) => {
@@ -53,6 +53,122 @@ export const getStatusEvaluationQuery = async (data: ArrayResidentsEvaluation) =
             )
 
             resolve(array);
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+export const getResidentsPerStatus = async ({ status = '', mes = '', ciclo = '', especialidad = '' }: PropsGetResidentsPerStatusQueries) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let data: any = [];
+
+            let reg = await db.ced_per_ciclo.findMany({
+                where: {
+                    ced_residentes: {
+                        ced_especialidades: {
+                            nombre: especialidad ? { contains: especialidad } : {}
+                        },
+                        status: 1 //active ones
+                    },
+                    ced_ciclo: {
+                        ciclo: ciclo,
+                    },
+                    deleted_at: null
+                },
+                select: {
+                    id_residente: true,
+                    grado_residente: true,
+                    ced_residentes: {
+                        select: {
+                            matricula: true,
+                            nombre: true,
+                            paterno: true,
+                            materno: true
+                        }
+                    }
+                }
+            });
+
+            const array = await Promise.all(
+                reg.map(async (residente: any) => {
+                    let estado: string = '';
+                    const reg = await db.ced_evaluacion.findFirst({
+                        where: {
+                            ced_periodo: {
+                                mes: mes,
+                                ced_ciclo: {
+                                    ciclo: ciclo
+                                }
+                            },
+                            ced_residentes: {
+                                id: residente.id_residente
+                            }
+                        },
+                        select: {
+                            grado_residente: true,
+                            en_rotacion: true,
+                            pendiente: true,
+                            id_modulo: true,
+                            id_residente: true,
+                            id_periodo: true,
+                            ced_residentes: {
+                                select: {
+                                    matricula: true,
+                                    nombre: true,
+                                    paterno: true,
+                                    materno: true
+                                }
+                            }
+                        }
+                    });
+
+                    if ((reg == null) || (reg.pendiente == 0 && reg.en_rotacion == 0 && reg.id_modulo == null)) {
+                        estado = 'evaluar';
+                    } else {
+                        if (reg.pendiente == 1 && reg.en_rotacion == 0) {
+                            estado = 'pendiente';
+                        } else if (reg.id_residente != null && reg.id_periodo != null && reg.id_modulo != null && reg.pendiente == 0) {
+                            estado = 'completado';
+                        } else if (reg.en_rotacion == 1) {
+                            estado = 'rotacion'
+                        }
+                    }
+
+                    return {
+                        matricula: reg != null ? reg.ced_residentes.matricula : residente.ced_residentes.matricula,
+                        nombre: reg != null ? reg.ced_residentes.nombre + ' ' + reg.ced_residentes.paterno + ' ' + reg.ced_residentes.materno : residente.ced_residentes.nombre + ' ' + residente.ced_residentes.paterno + ' ' + residente.ced_residentes.materno,
+                        grado: reg != null ? reg.grado_residente : residente.grado_residente,
+                        status: estado
+                    }
+                })
+            )
+
+            switch (status) {
+                case 'Completados':
+                    data = array.filter((res) => {
+                        return res.status == 'completado';
+                    });
+                    break;
+                case 'No Evaluados':
+                    data = array.filter((res) => {
+                        return res.status == 'evaluar';
+                    });
+                    break;
+                case 'Pendientes':
+                    data = array.filter((res) => {
+                        return res.status == 'pendiente';
+                    });
+                    break;
+                case 'En Rotación':
+                    data = array.filter((res) => {
+                        return res.status == 'rotacion';
+                    });
+                    break;
+            }
+
+            resolve(data);
         } catch (error) {
             reject(error);
         }
